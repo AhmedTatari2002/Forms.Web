250bedab71f8c2841f564566ec42ab7e01aa5970
Ahmed AL Tatari <ahmed.m.tatari@gmail.com>
2024-11-20 14:57:21 +0200

    Add files via upload

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,104 @@
+﻿using Forms.Web.Data;
+using Forms.Web.Models;
+using Forms.Web.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forms.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _db.Categories.Where(x => !x.IsDelete).ToList();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update(int Id)
+        {
+            var category = _db.Categories.SingleOrDefault(x => x.Id == Id && !x.IsDelete);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            var vm = new UpdateCategoryViewModel();
+            vm.Id = category.Id;
+            vm.Name = category.Name;
+            return View(vm);
+        }
+
+
+        [HttpPost]
+        public IActionResult Update(UpdateCategoryViewModel input)
+        {
+            if (ModelState.IsValid)
+            {
+                var category = _db.Categories.SingleOrDefault(x => x.Id == input.Id && !x.IsDelete);
+                if (category == null)
+                {
+                    return NotFound();
+                }
+                category.Name = input.Name;
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["msg"] = "s:Item Updated Succsfuly !";
+                return RedirectToAction("Index");
+            }
+            return View(input);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            var category = _db.Categories.SingleOrDefault(x => x.Id == Id && !x.IsDelete);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            category.IsDelete = true;
+            _db.Categories.Update(category);
+            _db.SaveChanges();
+
+            TempData["msg"] = "s:Item Deleted Succsfuly !";
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(CreateCategoryViewModel input)
+        {
+            if (ModelState.IsValid)
+            {
+                var category = new Category();
+                category.Name = input.Name;
+                category.CreatedAt = DateTime.Now;
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                TempData["msg"] = "Item Added Succsfuly !";
+                return RedirectToAction("Index");
+            }
+            return View(input);
+            //code to save in database
+          
+        }
+
+
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using Forms.Web.Data;
+using Forms.Web.Models;
+using Forms.Web.Services;
+using Forms.Web.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forms.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _db;
+        private IFileService _fileService;
+        private IEmailService _emailService;
+
+        public ProductController(ApplicationDbContext db,IFileService fileService, IEmailService emailService)
+        {
+            _db = db;
+            _fileService = fileService;
+            _emailService = emailService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _db.Products.Include(x => x.Category).Where(x => !x.IsDelete).OrderByDescending(x => x.CreatedAt).ToListAsync();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewData["categoryList"] =  new SelectList(_db.Categories.Where(x => !x.IsDelete).ToList(),"Id","Name");
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductViewModel input)
+        {
+            if (ModelState.IsValid)
+            {
+                //check 
+
+                var nameIsExist = _db.Products.Any(x => x.Name == input.Name && !x.IsDelete);
+                if (nameIsExist)
+                {
+                    TempData["msg"] = "e:Product Name is Already Exist!";
+                    ViewData["categoryList"] = new SelectList(_db.Categories.Where(x => !x.IsDelete).ToList(), "Id", "Name");
+                    return View(input);
+                }
+
+                var product = new Product();
+                product.Name = input.Name;
+                product.Description = input.Description;
+                product.Price = input.Price;
+                if(input.ImageUrl != null)
+                {
+                    product.ImageUrl = await _fileService.SaveFile(input.ImageUrl,"Images");
+                }
+                product.CategoryId = input.CategoryId;
+                product.CreatedAt = DateTime.Now;
+
+                _db.Products.Add(product);
+                _db.SaveChanges();
+
+                //send email to admins
+                var admins = _db.Admins.Where(x => !x.IsDelete).ToList();
+
+                foreach(var admin in admins)
+                {
+                   await  _emailService.Send(admin.Email, "New Product !", $"New Product With name : {product.Name}");
+                }
+
+                TempData["msg"] = "s:Product was added succsfuly!";
+
+                return RedirectToAction("Index");
+            }
+
+            ViewData["categoryList"] = new SelectList(_db.Categories.Where(x => !x.IsDelete).ToList(), "Id", "Name");
+            return View(input);
+        }
+    }
+}
